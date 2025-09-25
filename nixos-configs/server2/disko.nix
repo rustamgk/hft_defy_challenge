69ab37a42fd24721f002a663c258f89b4a8eb367
Rustam Galimyanov <rustam.gk@gmail.com>
2025-09-25 17:22:54 +0200

    [DEFY-001] :: Server1 and server2 disk configuration

---
+++ nixos-configs/server2/disko.nix
@@ -0,0 +1,38 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "128M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
