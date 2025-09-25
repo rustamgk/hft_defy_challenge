a190a4369d010b221c7492866df70d44fd7a1180
Rustam Galimyanov <rustam.gk@gmail.com>
2025-09-25 16:33:37 +0200

    [DEFY-001] :: Nixos config update

--- nixos-configs/flake.nix
+++ nixos-configs/flake.nix
@@ -1,5 +1,7 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+   description = "HFT DeFy Challenge - NixOS Configuration";
+  
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
@@ -11,59 +13,59 @@
       nixos-facter-modules,
       ...
     }:
+    
     {
-      nixosConfigurations.hetzner-cloud = nixpkgs.lib.nixosSystem {
+      server1 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
           ./configuration.nix
-        ];
-      };
-      # tested with 2GB/2CPU droplet, 1GB droplets do not have enough RAM for kexec
-      nixosConfigurations.digitalocean = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./digitalocean.nix
-          disko.nixosModules.disko
-          { disko.devices.disk.disk1.device = "/dev/vda"; }
-          ./configuration.nix
-        ];
-      };
-      nixosConfigurations.hetzner-cloud-aarch64 = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
-      };
-
-      # Use this for all other targets
-      # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
-      nixosConfigurations.generic = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-          ./hardware-configuration.nix
-        ];
-      };
-
-      # Slightly experimental: Like generic, but with nixos-facter (https://github.com/numtide/nixos-facter)
-      # nixos-anywhere --flake .#generic-nixos-facter --generate-hardware-config nixos-facter facter.json <hostname>
-      nixosConfigurations.generic-nixos-facter = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-          nixos-facter-modules.nixosModules.facter
+          ./server1/configuration.nix
+          ./common/base.nix
+          ./common/tunnels.nix
           {
-            config.facter.reportPath =
-              if builtins.pathExists ./facter.json then
-                ./facter.json
-              else
-                throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
+            users.users.root.openssh.authorizedKeys.keys = [
+              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCQyMU1puQduxR17dyVjLEA8PlUT8yhe7WyauG8EERSviQJAnPKT9fER+FCrDK9/63xmFPDXaNU/V4W7Qm91SkAaaYE9sbi9+M00pU4pgNvuIsNe/iRgqWFsEWLJwLOzD67rCy/XriqtZbVUXhE36UlCsLYaADOUjzvjT4XX9ktkvSyjA/bCs2qUPlc+j76tioTn0b9aZaMcJ0AgpBGXDQKcoNAXyMX3mlJq7ARHaDNLSak2ZM0mKa5XREYp0XukD8Eb9Kuf+0k7gCVmkuJU+fkzH6F+/DoMZYMrc9s9Fzn9+HBEVQhGv7nDLS2e+LGVmTTtMJDyc8SMYBpe42Bupo5AqzMbG3ifL6xFbvnyb5ZN/91vlBNR3K2HXziqBWwPSgyJs8ExVp/yfNRUvEd4SDRv6E8ff8KJWIe5e/8qeYddpfFN2G5qjbGlTvCDhYijn+ckn71p+7YQI0gifQafiAb2X3z970+vXRPeJnKC9s+N+s1xnO/Xwqxo/LN2xGGIBaysXpT8lJyXiJvohkrpQD0hlvcfYnvfF3RZzkEdzqKJi2ISA7N+Wqz+jjGsqE60VP5cGgtkoHm04ayCVrIwiNEWGQ90mI6D8Wph3cRhPDZcNjH1qQF6Swj+/K499hk2pCWP3rxfsfnErGsKxf2brqpfx5TM9T9k8ATuWnqkAwCXw== rustamgk@zbook-fury-g8"
+            ];
+            
+            # Enable SSH
+            services.openssh = {
+              enable = true;
+              settings.PermitRootLogin = "yes";
+            };
+            
+            # Set hostname
+            networking.hostName = "server1";
+
           }
         ];
       };
+      
+      server2 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./configuration.nix
+          ./server1/configuration.nix
+          ./common/base.nix
+          ./common/tunnels.nix
+          {
+            users.users.root.openssh.authorizedKeys.keys = [
+              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCQyMU1puQduxR17dyVjLEA8PlUT8yhe7WyauG8EERSviQJAnPKT9fER+FCrDK9/63xmFPDXaNU/V4W7Qm91SkAaaYE9sbi9+M00pU4pgNvuIsNe/iRgqWFsEWLJwLOzD67rCy/XriqtZbVUXhE36UlCsLYaADOUjzvjT4XX9ktkvSyjA/bCs2qUPlc+j76tioTn0b9aZaMcJ0AgpBGXDQKcoNAXyMX3mlJq7ARHaDNLSak2ZM0mKa5XREYp0XukD8Eb9Kuf+0k7gCVmkuJU+fkzH6F+/DoMZYMrc9s9Fzn9+HBEVQhGv7nDLS2e+LGVmTTtMJDyc8SMYBpe42Bupo5AqzMbG3ifL6xFbvnyb5ZN/91vlBNR3K2HXziqBWwPSgyJs8ExVp/yfNRUvEd4SDRv6E8ff8KJWIe5e/8qeYddpfFN2G5qjbGlTvCDhYijn+ckn71p+7YQI0gifQafiAb2X3z970+vXRPeJnKC9s+N+s1xnO/Xwqxo/LN2xGGIBaysXpT8lJyXiJvohkrpQD0hlvcfYnvfF3RZzkEdzqKJi2ISA7N+Wqz+jjGsqE60VP5cGgtkoHm04ayCVrIwiNEWGQ90mI6D8Wph3cRhPDZcNjH1qQF6Swj+/K499hk2pCWP3rxfsfnErGsKxf2brqpfx5TM9T9k8ATuWnqkAwCXw== rustamgk@zbook-fury-g8"
+            ];
+            
+            # Enable SSH
+            services.openssh = {
+              enable = true;
+              settings.PermitRootLogin = "yes";
+            };
+            
+            # Set hostname
+            networking.hostName = "server1";
+
+          }
+        ];
+      };
+
     };
 }
